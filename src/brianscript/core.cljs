0b90bc171454e49adae3895d4112906fff1f6d72
Lau Jensen <lau.jensen@bestinclass.dk>
2014-10-23 15:07:08 +0200

    Tested int-array, down from 340ms to 140ms

--- src/brianscript/core.cljs
+++ src/brianscript/core.cljs
@@ -1,4 +1,5 @@
 (ns brianscript.core
+  (:use-macros [brianscript.macros :only [forloop]])
   (:require [domina :as domina]))
 
 (enable-console-print!)
@@ -9,56 +10,88 @@
 (def dim-screen  [(.-width canvas) (.-height canvas)])
 (def dim-scale   (vec (map / dim-screen dim-board)))
 
+(def off   0)
+(def dying 1)
+(def on    2)
+
 (defn fmap [f coll] (doall (map f coll)))
 
-(defn render-cell [ctx cell]
-  (let [[state x y] cell
-        x  (inc (* x (dim-scale 0)))
-        y  (inc (* y (dim-scale 1)))]
-    (set! (.-fillStyle ctx) (if (= state :dying) "#bbb" "#fff"))
-    (.fillRect ctx x y (dec (dim-scale 0)) (dec (dim-scale 1)))))
+(defn render-cell [ctx x y col]
+  (let [sx  (inc (* x (dim-scale 0)))
+        sy  (inc (* y (dim-scale 1)))]
+    (set! (.-fillStyle ctx) col)
+    (.fillRect ctx sx sy (dec (dim-scale 0)) (dec (dim-scale 1)))))
 
 (defn render [ctx stage]
   (set! (.-fillStyle ctx) "#000")
   (.fillRect ctx 0 0 (dim-screen 0) (dim-screen 1))
-  (fmap (fn [col]
-          (fmap #(when (not= :off (% 0))
-                   (render-cell ctx %)) col)) stage))
+  (forloop [(y 0) (< y (dim-board 1)) (inc y)]
+     (forloop [(x 0) (< x (dim-board 0)) (inc x)]
+         (condp = (get-state x y stage)
+           on      (render-cell ctx x y "#fff")
+           dying   (render-cell ctx x y "#bbb")
+           nil))))
 
-(def board
-     (for [x (range (dim-board 0))]
-       (for [y (range (dim-board 1))]
-         [(if (< 50 (rand-int 100)) :on :off) x y])))
+(defn make-board
+  [w h]
+  (let [board (int-array h)]
+    (forloop [(y 0) (< y h) (inc y)]
+       (let [col   (int-array w)]
+         (dotimes [x w]
+           (aset col x
+                 (if (< 50 (rand-int 100))
+                   on
+                   off)))
+         (aset board y col)))
+    board))
 
-(defn active-neighbors [above [left _ right] below]
-  (count
-   (filter #(= :on (% 0))
-           (concat above [left right] below))))
+(defn get-state
+  [x y board]
+  (let [dimy (count board)
+        dimx (count (aget board 0))]
+    (aget board (mod y dimy)
+                (mod x dimx))))
 
-(defn torus-window [coll]
-  (partition 3 1 (concat [(last coll)] coll [(first coll)])))
+(defn active-neighbors
+  [x y blocal]
+  (let [coords [[-1 -1] [0 -1] [1 -1]
+                [-1 0]         [1  0]
+                [-1 1]  [0 1]  [1  1]]]
+    (->> (map (fn [[xd yd]] ;delta x, deltay
+                (let [nx (+ x xd)  ;new x
+                      ny (+ y yd)] ; new y
+                  (get-state nx ny blocal)))
+              coords)
+         (filter #{on})
+         count)))
 
-(defn rules [above current below]
-  (let [[self x y]  (second current)]
-    (cond
-      (= :on    self)                              [:dying x y]
-      (= :dying self)                              [:off   x y]
-      (= 2 (active-neighbors above current below)) [:on    x y]
-      :else                                        [:off   x y])))
+(defn stepfn
+  [b]
+  (let [board @b
+        dimy (count board)
+        dimx (count (aget board 0))
+        newb (make-board dimx dimy)]
+    (forloop [(y 0) (< y dimy) (inc y)]
+       (forloop [(x 0) (< x dimx) (inc x)]
+          (let [current-state (get-state x y board)]
+            (aset newb y x
+                  (cond
+                   (= on current-state)               dying
+                   (= dying current-state)            off
+                   (= 2 (active-neighbors x y board)) on
+                   :else off)))))
+    (reset! b newb)))
+; does not set b
 
-(defn step [board]
-  (doall
-   (map (fn [window]
-          (apply #(doall (apply map rules %&))
-                 (doall (map torus-window window))))
-        (torus-window board))))
-
-(def stage (atom board))
+(def board (atom (make-board 90 90)))
 
 (defn main-loop
   []
-  (render primaryCtx (swap! stage step)))
+  (render primaryCtx (stepfn board)))
 
-(def thread (js/setInterval main-loop 500))
-
+;(def thread (js/setInterval main-loop 150))
 ;(js/clearInterval thread)
+
+(time (render primaryCtx (stepfn board)))
+
+;rules not working
